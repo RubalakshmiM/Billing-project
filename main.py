14b8f9712bbe6ec33a050e5ef50d8bdb37b01322
RubalakshmiM <47208896+RubalakshmiM@users.noreply.github.com>
2020-06-18 12:32:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import xml.etree.ElementTree as ET
+itemlist=[]
+pricelist=[]
 tree=ET.parse("product list.xml")
 root=tree.getroot()
-itemList=[]
-n=int(input(" enter the total items:"))
-for i in range(0,n):
-  a=input(" enter name of the item:")
-  itemList.append(a)
-dict={}
-for x in root.findall('item'):
-    for storedItems in itemList:
-        if storedItems==x.find('name').text:
-            dict[storedItems]=x.find('price').text
-bill=0
-for key,value in dict.items():
-    quantity=int(input(" The quantity of " +key+ " purchased  is  = "))
-    totalprice=int(quantity * int(value))
-    bill=bill+totalprice
-    print(" Cost of single quantity of "+key+ " is =  ",value, " and  the total price of "+key+" is  = ",totalprice  )
-print(" Overall bill generated is = ",bill)
+for child in root:
+    itemlist.append(child.find('name').text)
+    pricelist.append(child.find('price').text)
+dicti={}
+dicti = dict(zip(itemlist,pricelist))
+n=[]
+s=int(input(" enter the total item:"))
+for i in range(0,s):
+      key=input(" enter the name:")
+      if key not in dicti:
+            x=input(" do yu want to add this item in dict (y/n)")
+            if x=='y':
+                   price=int(input(" enter the price you want to add for the item "))
+                   dicti.update({key: price})
+            else:
+               if x=='n':
+                    print(" thankyou")
+            n.append(key)
+print(dicti)
+
 
 
 
